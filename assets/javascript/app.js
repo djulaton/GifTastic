c903f466cb77dc0f5ad44bf54a8d7c9e4621e1ac
Diana <dianajulaton@gmail.com>
2018-06-23 21:53:03 -0700

    some updates

---
+++ assets/javascript/app.js
@@ -0,0 +1,22 @@
+//form is displayed on screen with an input and submit button
+
+//there are also buttons that appear on the screen from the character array
+
+//each button will display max 10 gifs and ratings
+
+//all gifs will be still unless user clicks on the image
+
+//when the image is clicked, the image will animate
+
+//when clicked again, image will be still
+
+//when user inputs a choice and submits, add button to screen and allow them to click the added button and view 10 max images and display ratings
+
+//create global variables
+
+var characterArray = ["black ranger", "pink ranger", "yellow ranger", "green ranger", "red ranger", "blue ranger", "rita repulsa", "zordon", "alpha 5", "bulk and skull"];
+
+function displayCharacters () {
+    
+}
+

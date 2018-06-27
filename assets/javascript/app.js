8beea0564f800d13df735a55ef5dd11b60b8e2c9
Diana <dianajulaton@gmail.com>
2018-06-27 00:10:41 -0700

    still images are working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -16,15 +16,65 @@
 
 var characterArray = ["black ranger", "pink ranger", "yellow ranger", "green ranger", "red ranger", "blue ranger", "rita repulsa", "zordon", "alpha 5", "bulk and skull"];
 
-var queryURL = "https://api.giphy.com/v1/gifs/search?&q=" + characterArray + 
+function displayRangerInfo() {
+
+    var ranger = $(this).attr("data-name");
+    var queryURL = "https://api.giphy.com/v1/gifs/search?&q=" + ranger + 
 "&rating=PG&lang=en&api_key=bLGuPuEQa3pGIFR5h8MvxFwOFQg9ScJc&limit=10";
+
+// creating an AJAX call for the specific ranger button being clicked
+$.ajax({
+    url: queryURL,
+    method: "GET"
+}).then(function(response){
+
+    var results = response.data;
+
+    for(var i=0; i < results.length; i++) {
+
+    // creating a div to hold the ranger result
+    var rangerDiv = $("<div class='ranger'>");
+
+    // storing the rating data
+    var rating = results[i].rating;
+
+    // Creating an element to have the rating displayed
+    var pOne = $("<p>").text("Rating: " + rating);
+
+    // Displaying the rating
+    rangerDiv.append(pOne);
+
+    // retrieving the URL for the image
+    var imgURL = results[i].images.fixed_height_still.url;
+
+    var image = $("<img>").attr("src", imgURL);
+
+    // appending the image
+    rangerDiv.append(image);
+
+    // putting the entire ranger image aboe the previouse gifs
+    $("#powerRangers-view").prepend(rangerDiv);
+    }
+});
+
+}
 
 // function for displaying movie data
 function renderButtons() {
-    $("#powerRangers-view").empty();
+    $("#buttons-view").empty();
 
     for(var i=0; i < characterArray.length; i++) {
-      $("#powerRangers-view").append("<button>" + characterArray[i] + "</button>")
+    //   $("#powerRangers-view").append("<button>" + characterArray[i] + "</button>")
+
+    var a = $("<button>");
+    //adding a class of ranger-btn to button
+    a.addClass("ranger-btn");
+    // adding a data-attribute
+    a.attr("data-name", characterArray[i]);
+    // providing the initial button text
+    a.text(characterArray[i]);
+    // adding the button to the buttons-view div
+    $("#buttons-view").append(a);
     }
 }
 
@@ -36,6 +86,9 @@ $("#add-ranger").on("click", function(event){
 
     renderButtons();
 });
+
+// adding a click even listener to all elements with a class of "ranger-btn"
+$(document).on("click", ".ranger-btn", displayRangerInfo);
 
 renderButtons();
 
